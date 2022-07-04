a18366e84fb2249647fc3d7b3e7a4058140a7a94
woodyverse <metaverso@woodyverso.com.br>
2022-07-04 18:49:51 -0300

    update com botoes e seta

--- hello-world/src/app.ts
+++ hello-world/src/app.ts
@@ -1,31 +1,22 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
-
-/**
- * The main class of this app. All the logic goes here.
- */
 export default class HelloWorld {
-	private text: MRE.Actor = null;
-	private cube: MRE.Actor = null;
+	private texto: MRE.Actor = null;
+	private BotaoVermelho: MRE.Actor = null;
+	private seta: MRE.Actor = null;
+	private BotaoAzul: MRE.Actor = null;
 	private assets: MRE.AssetContainer;
 
 	constructor(private context: MRE.Context) {
 		this.context.onStarted(() => this.started());
 	}
-
-	/**
-	 * Once the context is "started", initialize the app.
-	 */
 	private async started() {
-		// set up somewhere to store loaded assets (meshes, textures, animations, gltfs, etc.)
 		this.assets = new MRE.AssetContainer(this.context);
 
-		// Create a new actor with no mesh, but some text.
-		this.text = MRE.Actor.Create(this.context, {
+		const botaoVermelho = await this.assets.loadGltf('Botao_Vermelho.glb', "box");
+		const botaoAzul = await this.assets.loadGltf('Botao_Azul.glb', "box");
+		const setaAsset = await this.assets.loadGltf('Seta.glb', "box");
+
+		this.texto = MRE.Actor.Create(this.context, {
 			actor: {
 				name: 'Text',
 				transform: {
@@ -39,44 +30,23 @@ export default class HelloWorld {
 				}
 			}
 		});
-
-		// Here we create an animation for our text actor. First we create animation data, which can be used on any
-		// actor. We'll reference that actor with the placeholder "text".
 		const spinAnimData = this.assets.createAnimationData(
-			// The name is a unique identifier for this data. You can use it to find the data in the asset container,
-			// but it's merely descriptive in this sample.
 			"Spin",
 			{
-				// Animation data is defined by a list of animation "tracks": a particular property you want to change,
-				// and the values you want to change it to.
 				tracks: [{
-					// This animation targets the rotation of an actor named "text"
 					target: MRE.ActorPath("text").transform.local.rotation,
-					// And the rotation will be set to spin over 20 seconds
 					keyframes: this.generateSpinKeyframes(20, MRE.Vector3.Up()),
-					// And it will move smoothly from one frame to the next
 					easing: MRE.AnimationEaseCurves.Linear
 				}]
 			});
-		// Once the animation data is created, we can create a real animation from it.
 		spinAnimData.bind(
-			// We assign our text actor to the actor placeholder "text"
-			{ text: this.text },
-			// And set it to play immediately, and bounce back and forth from start to end
+			{ text: this.texto },
 			{ isPlaying: true, wrapMode: MRE.AnimationWrapMode.PingPong });
-
-		// Load a glTF model before we use it
-		const cubeData = await this.assets.loadGltf('Botao_Vermelho.glb', "box");
-
-		// spawn a copy of the glTF model
-		this.cube = MRE.Actor.CreateFromPrefab(this.context, {
-			// using the data we loaded earlier
-			firstPrefabFrom: cubeData,
-			// Also apply the following generic actor properties.
+		this.BotaoAzul = MRE.Actor.CreateFromPrefab(this.context, {
+			firstPrefabFrom: botaoAzul,
 			actor: {
-				name: 'Altspace Cube',
-				// Parent the glTF model to the text actor, so the transform is relative to the text
-				parentId: this.text.id,
+				name: 'botao plateia',
+				parentId: this.texto.id,
 				transform: {
 					local: {
 						position: { x: 0, y: -1, z: 0 },
@@ -85,55 +55,70 @@ export default class HelloWorld {
 				}
 			}
 		});
-
-		// Create some animations on the cube.
+		this.BotaoVermelho = MRE.Actor.CreateFromPrefab(this.context, {
+			firstPrefabFrom: botaoVermelho,
+			actor: {
+				name: 'botaoADM',
+				parentId: this.texto.id,
+				transform: {
+					local: {
+						position: { x: 4, y: 3, z: 0 },
+						scale: { x: 0.8, y: 0.8, z: 0.8 }
+					}
+				}
+			}
+		});
+		
 		const flipAnimData = this.assets.createAnimationData(
-			// the animation name
 			"DoAFlip",
 			{ tracks: [{
-				// applies to the rotation of an unknown actor we'll refer to as "target"
 				target: MRE.ActorPath("target").transform.local.rotation,
-				// do a spin around the X axis over the course of one second
 				keyframes: this.generateSpinKeyframes(1.0, MRE.Vector3.Right()),
-				// and do it smoothly
 				easing: MRE.AnimationEaseCurves.Linear
 			}]}
 		);
-		// apply the animation to our cube
-		const flipAnim = await flipAnimData.bind({ target: this.cube });
+		const flipAnim = await flipAnimData.bind({ target: this.BotaoAzul });
+		const buttonBehavior = this.BotaoAzul.setBehavior(MRE.ButtonBehavior);
 
-		// Set up cursor interaction. We add the input behavior ButtonBehavior to the cube.
-		// Button behaviors have two pairs of events: hover start/stop, and click start/stop.
-		const buttonBehavior = this.cube.setBehavior(MRE.ButtonBehavior);
-
-		// Trigger the grow/shrink animations on hover.
 		buttonBehavior.onHover('enter', () => {
-			// use the convenience function "AnimateTo" instead of creating the animation data in advance
-			MRE.Animation.AnimateTo(this.context, this.cube, {
+			MRE.Animation.AnimateTo(this.context, this.BotaoAzul, {
 				destination: { transform: { local: { scale: { x: 0.5, y: 0.5, z: 0.5 } } } },
 				duration: 0.3,
 				easing: MRE.AnimationEaseCurves.EaseOutSine
 			});
 		});
 		buttonBehavior.onHover('exit', () => {
-			MRE.Animation.AnimateTo(this.context, this.cube, {
+			MRE.Animation.AnimateTo(this.context, this.BotaoAzul, {
 				destination: { transform: { local: { scale: { x: 0.4, y: 0.4, z: 0.4 } } } },
 				duration: 0.3,
 				easing: MRE.AnimationEaseCurves.EaseOutSine
 			});
 		});
+		buttonBehavior.onClick(usuario => {
 
-		// When clicked, do a 360 sideways.
-		buttonBehavior.onClick(_ => {
+			this.seta = MRE.Actor.CreateFromPrefab(this.context, {
+				firstPrefabFrom: setaAsset,
+				actor: {
+					name: 'botaoADM',
+					parentId: this.texto.id,
+					transform: {
+						local: {
+							position: { x: 4, y: 3, z: 0 },
+							scale: { x: 0.8, y: 0.8, z: 0.8 }
+						}
+					},
+					attachment: {
+						attachPoint: 'head', 
+						userId: usuario.id
+						
+					}
+				}
+			});
+			
+			this.texto.text.contents = "Direito de resposta para:" + usuario.name;
 			flipAnim.play();
 		});
 	}
-
-	/**
-	 * Generate keyframe data for a simple spin animation.
-	 * @param duration The length of time in seconds it takes to complete a full revolution.
-	 * @param axis The axis of rotation in local space.
-	 */
 	private generateSpinKeyframes(duration: number, axis: MRE.Vector3): Array<MRE.Keyframe<MRE.Quaternion>> {
 		return [{
 			time: 0 * duration,
